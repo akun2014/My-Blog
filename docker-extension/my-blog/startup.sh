66afee9e8ab79f1bcf19bb94bc540b825f26b632
akun <e-gk@qq.com>
2019-05-05 00:30:36 +0800

    update

--- docker-extension/my-blog/startup.sh
+++ docker-extension/my-blog/startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-sleep 25
-
 echo '开始启动项目.....'
-nohup java -jar /app.jar --spring.profiles.active=default -Djasypt.encryptor.password=${JASYPT_PASSWORD}
+nohup java -jar /app.jar --spring.profiles.active=default
+#-Djasypt.encryptor.password=${JASYPT_PASSWORD}
